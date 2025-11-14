ea0374f339f2dbdd71ecf887d9610baa6a691f50
Sneha Saha <souvik9874721150@gmail.com>
2025-11-14 23:42:58 +0530

    Filtering columns

---
+++ L7.py
@@ -0,0 +1,19 @@
+# Filtering columns based on some condition
+
+import pandas as pd
+
+data ={
+    "Name" : ['Ram','Shyam','Amit','Aditi','Karan','Aditya','Sonia'],
+    "Age" : [25,21,36,28,48,19,20],
+    "City" : ['Nagpur','Mumbai','Delhi','Panjab','UP','Bihar','Kolkata'],
+    "Salary":[45000,52000,60000,49000,75000,30000,35000],
+    "Performance Score":[85,79,82,87,98,92,80]
+}
+
+df = pd.DataFrame(data)
+
+print(df['Name'])   
+
+subset = df[["Name","Age"]]
+
+print(subset)
