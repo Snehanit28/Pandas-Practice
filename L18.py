03117156206d13f76ea155b9a1b9da470b290ca7
Sneha Saha <souvik9874721150@gmail.com>
2025-12-02 23:36:48 +0530

    Use of mean(),max(),min(),sum()

--- L18.py
+++ L18.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 data = {
     "Name" : ['Ram','Shyam','Amit','Aditi','Karan','Aditya','Sonia'],
-    "Age" : [25,21,36,28,48,19,20],
+    "Age" : [28,21,36,28,48,19,20],
     "Salary":[55000,50000,60000,39000,75000,50000,31000]
 }
 df = pd.DataFrame(data)
@@ -19,8 +19,19 @@ print(grouped)
             # => 19    50000
             # => 20    31000
             # => 21    50000
-            # => 25    55000
-            # => 28    39000
+            # => 28    94000
             # => 36    60000
             # => 48    75000
+            # => Name: Salary, dtype: int64# => 19    50000
+
+grouped1 = df.groupby(["Age","Name"])["Salary"].sum()
+print(grouped1)
+            # => Age  Name  
+            # => 19   Aditya    50000
+            # => 20   Sonia     31000
+            # => 21   Shyam     50000
+            # => 28   Aditi     39000
+            # =>      Ram       55000
+            # => 36   Amit      60000
+            # => 48   Karan     75000
             # => Name: Salary, dtype: int64
